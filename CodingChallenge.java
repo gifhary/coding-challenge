d10130dfa022e1af6cad8691d4081d0951f639a9
Gifhary Syidhqa Hamim <gifhary.1997@gmail.com>
2019-11-05 20:33:21 +0800

    added documentation

--- CodingChallenge.java
+++ CodingChallenge.java
@@ -8,14 +8,17 @@ public class CodingChallenge {
 
         Scanner scan = new Scanner(System.in);
 
+        //Set how many digits number after comma
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(5);
 
+        //Base number to be rooted, accept more than 17 digits
         System.out.print("Enter the base number : ");
-        double baseNum = scan.nextDouble(); // Base number to be rooted, accept more than 17 digits
+        double baseNum = scan.nextDouble();
 
+        //nth root number (only 1-10)
         System.out.print("Enter nth root number : ");
-        int rootNum = scan.nextInt(); // nth root number (only 1-10)
+        int rootNum = scan.nextInt();
 
         scan.close();
 
@@ -33,12 +36,29 @@ public class CodingChallenge {
 
     }
 
+    /*
+     * This method is used to find the nth root of any number with requirement :
+     * Takes in two parameters: a number and a root.
+     * Accept up to 17 digits number
+     * Able to calculate nth root between 1 and 10
+     * Not using any math library to compute
+     * Not using any special operator
+     * 
+     * This method work using nth root number input as aguide to select which code to process
+     * Most code is using similar method to compute the nth root of a number except for 1st and 2nd root
+     * The method used here is manually increasing index number iteratively, compute "index" to the "nth root"
+     * power and then assign the value as reference. As long the reference value still less than the base
+     * number, the loop keep increasing the index value until "reference" is equal to "base"
+     * The method will retun final index value as the nth root of base number
+    */
     public static double nthRoot(double base, int root) {
-        double i = 1, result = 1;
+        double i = 1; //index
+        double reference = 1;
         double addition = 0.000001; // smaller number is better but take more time to process large base number
 
         switch (root) {
         case 1:
+        //If the user input for nth root is 1 (one), it return the base number itself
             i = base;
             break;
 
@@ -56,62 +76,63 @@ public class CodingChallenge {
             break;
 
         case 3:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i;
+                reference = i * i * i;
             }
             break;
 
         case 4:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i;
+                reference = i * i * i * i;
             }
             break;
 
         case 5:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i;
+                reference = i * i * i * i * i;
             }
             break;
 
         case 6:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i * i;
+                reference = i * i * i * i * i * i;
             }
             break;
 
         case 7:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i * i * i;
+                reference = i * i * i * i * i * i * i;
             }
             break;
 
         case 8:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i * i * i * i;
+                reference = i * i * i * i * i * i * i * i;
             }
             break;
 
         case 9:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i * i * i * i * i;
+                reference = i * i * i * i * i * i * i * i * i;
             }
             break;
 
         case 10:
-            while (result < base) {
+            while (reference < base) {
                 i = i + addition;
-                result = i * i * i * i * i * i * i * i * i * i;
+                reference = i * i * i * i * i * i * i * i * i * i;
             }
             break;
 
         default:
+        //nth root number other than 1-10, it throws an exception error
             throw new RejectedExecutionException("Allowed root number 1-10 only");
         }
         return i - addition;
